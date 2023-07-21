8ebae6491e6cc163f3a24f54a2c6920a7a419f6c
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2023-07-21 15:01:09 +0200

    prettier DID print

--- wallet/src/App.tsx
+++ wallet/src/App.tsx
@@ -4,19 +4,23 @@ import * as vc from "./assets/academic.json";
 import VerifiableCredential from "./components/VerifiableCredential";
 import { generateDID, resolveDID } from "./utils/did";
 import { createRandomBytes } from "./utils/randomBytes";
+import * as key from "./assets/jwk.json";
 
 function App() {
   const [DID, setDID] = useState("");
   const [valid, setValid] = useState<string | undefined>("");
+  const [DIDdoc, setDIDdoc] = useState("");
 
   function handleGenerateDID() {
-    // setDID(generateDID(createRandomBytes(16)));
     const jwk = {
-      crv: "P-256",
-      kty: "EC",
-      x: "5cwlfgppG78lgAuQINMjfuSzYuMmlMMPvsaW9rrpY-Q",
-      y: "EKKMutKdsLijKIGw1394cmnrEDzURkeJBZWpt28QfAg",
+      kty: key.kty,
+      crv: key.crv,
+      kid: key.kid,
+      x: key.x,
+      y: key.y,
     };
+    console.log(jwk);
+    // setDID(generateDID(createRandomBytes(16)));
     setDID(generateDID(jwk));
   }
 
@@ -24,11 +28,15 @@ function App() {
     resolveDID(DID)
       // resolveDID("did:ebsi:ziDnioxYYLW1a3qUbqTFz4W")
       // .then((res) => console.log(res))
-      .then((res) =>
-        res.didDocument === null
-          ? setValid(res.didResolutionMetadata.error)
-          : setValid(res.didDocument.id)
-      )
+      .then((res) => {
+        if (res.didDocument === null) {
+          setValid(res.didResolutionMetadata.error);
+        } else {
+          setValid(res.didDocument.id);
+          setDIDdoc(JSON.stringify(res.didDocument, null, 2));
+          console.log(res.didDocument);
+        }
+      })
       .catch(() => "error");
   }
 
@@ -54,6 +62,13 @@ function App() {
       <pre>
         <br />
         {valid ? valid : "-"}
+      </pre>
+      <pre>
+        <br />
+        <b>DID Document:</b>
+        <br />
+        <br />
+        {DIDdoc ? DIDdoc : "-"}
       </pre>
     </>
   );
