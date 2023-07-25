9e2bf7ad8d71995e0314c62d73c855f5715c269f
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2023-07-25 14:39:16 +0200

    added manifest and robots.txt

--- wallet/vite.config.ts
+++ wallet/vite.config.ts
@@ -11,6 +11,41 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      includeAssets: [
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon-180x180.png",
+      ],
+      manifest: {
+        name: "Wallet",
+        short_name: "Wallet",
+        description: "Wallet for Verifiable Credentials",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "wallet-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "wallet-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "wallet-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "wallet-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
     }),
   ],
 });
