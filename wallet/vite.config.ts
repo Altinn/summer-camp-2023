a46c2a3a64f2093875b8b17e4a5071e3e007b5ce
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2023-07-25 12:30:29 +0200

    configs
    fixed eslint config, and added base config for pwa

--- wallet/vite.config.ts
+++ wallet/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
