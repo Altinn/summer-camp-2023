5b4d7b8f99443488a1566c58d4da3bdeb7b4b035
Emil Ovesen <emilk2001@gmail.com>
2023-08-02 12:03:53 +0200

    add verifyer to server (not working yet)

--- backend/scripts/server/index.js
+++ backend/scripts/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ethers = require('ethers');
 const path = require('path');
 const fs = require('fs');
-const {registerCredential} = require("./services/registerCredential.js");
+const {registerCredential, verifyCredential} = require("./services/registerCredential.js");
 const exp = require('constants');
 
 const app = express();
@@ -14,17 +14,7 @@ app.use(express.json());
 // Address of claims_Verifyer
 const VERIFYER_ADRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
 
-
-app.post('/register', async (req, res) => {
-    try {
-
-        const { did, firstName, lastName, expDate, location, municipality, alcoGroup } = req.body;
-        console.log(did)
-        if(!did) {
-            return res.status(400).json({error: "DID is required"})
-        }
-
-        const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
+const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
       provider.getBlockNumber().then((result) => {
           console.log("Current block number: " + result);
       })
@@ -36,6 +26,24 @@ app.post('/register', async (req, res) => {
       const abiVerifier = JSON.parse(fs.readFileSync(abiPathVerifier).toString()).abi;
       
       const contract = new ethers.Contract(VERIFYER_ADRESS, abiVerifier, wallet);
+
+
+app.post('/register', async (req, res) => {
+    try {
+
+        const { did, firstName, lastName, expDate, location, municipality, alcoGroup } = req.body;
+        console.log(did)
+
+        //save data to a local file
+        const jsonData = JSON.stringify(req.body);
+        fs.writeFileSync('data.json', jsonData);
+        console.log("Data saved in local file successfully");
+
+        if(!did) {
+            return res.status(400).json({error: "DID is required"})
+        }
+
+        
   
       const credential = await registerCredential(did, firstName, lastName, expDate, location, municipality, alcoGroup, contract);
       res.status(201);
@@ -45,23 +53,18 @@ app.post('/register', async (req, res) => {
         res.send(error);
     }
     
-  })
-
-// POST endpoint to save data to a file
-  app.post('/save', (req, res) => {
-    try {
-
-
-      const jsonData = JSON.stringify(req.body);
-
-      fs.writeFileSync('data.json', jsonData);
-
-      res.status(200).send('Data saved successfully');
-    } catch (error) {
-      console.error('Error saving data', error);
-      res.status(500).send('Error saving data')
-    }
   });
+
+app.post('/verify', async (req, res) => {
+  const { vc } = req.body;
+  console.log(vc);
+  const result = await verifyCredential(vc, contract);
+  if(result[0] == true && result[1] ==true && result[2] ==true && result[3] ==true && result[4] ==true){
+    res.send(true);
+  }
+  res.send(false);
+})
+
 // DELETE endpoint to cleqar the file
   app.delete('/clear', (req, res) => {
     try {
